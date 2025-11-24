f72f8324d3b0bb01e6059149ede4c1cac3db492b
mariammmramy <mariam.said06@eng-st.cu.edu.eg>
2025-11-24 22:45:45 +0200

    some data structures

--- headers.h
+++ headers.h
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include <string.h>
+#include "queue.c"
 
 typedef short bool;
 #define true 1
@@ -111,6 +112,7 @@ void printLinkedlist(struct Node *p) {
   }
 }
 
+
 //enum for process state
 typedef enum {READY, RUNNING, FINISHED } proc_state;
 
@@ -127,44 +129,5 @@ struct PCB {
     int waiting_time;  
     proc_state state;  //one of the enums 0,1,2
 };
-
-/////enqueue and dequeue priority queue for strn
-
-bool enqueueSTRN(struct Node** head, struct Process p)
-{
-    struct Node* newNode = malloc(sizeof(struct Node));  //dynamically allocate space to node
-    if (!newNode) return false;
-
-    newNode->P = p;
-    newNode->next = NULL;
-
-    if (*head == NULL || p.RemainingTime < (*head)->P.RemainingTime)  //if process remaining time is shortest than                                                                 
-    {                                                                 //head or no head
-        newNode->next = *head;
-        *head = newNode;
-        return true;
-    }
-
-    struct Node* cur = *head;
-    while (cur->next != NULL && cur->next->P.RemainingTime <= p.RemainingTime)  //loop until remainig time is less than next
-        cur = cur->next;
-
-    newNode->next = cur->next;
-    cur->next = newNode;
-    return true;
-}
-
-bool dequeue(struct Node** head, struct Process* out)   
-{
-    if (*head == NULL) return false;
-
-    struct Node* temp = *head;
-    *out = temp->P;
-
-    *head = (*head)->next;
-    free(temp);
-    return true;
-}
-
 
 #endif
