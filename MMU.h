b80b09475bfee03c752cc3447124c12245d7bdcd
Mennatullah <mennatullahmohammad99@gmail.com>
2025-12-21 14:08:47 +0200

    added MMU and makefile

---
+++ MMU.h
@@ -0,0 +1,36 @@
+#ifndef MMU_H
+#define MMU_H
+
+#define MEMORY_SIZE 512          // Total RAM size in bytes
+#define PAGE_SIZE 16             // Page size in bytes
+#define FRAME_COUNT (MEMORY_SIZE / PAGE_SIZE)  // 32 frames
+
+
+typedef struct {
+    int free;        // 1 = frame is free, 0 = not free
+    int pid;         // Process ID that owns this frame
+    int vpn;         // Virtual Page Number stored in this frame
+    int ref;         // Reference bit 
+    int modified;    // 1 if page was written 
+} Frame;
+
+
+
+typedef struct {
+    int valid;       // 1 = page is in memory
+    int frame;       // Physical frame number
+    int ref;         // Reference bit
+    int modified;    // Modified bit
+} PageTableEntry;
+
+
+extern Frame frame_table[FRAME_COUNT];
+
+
+  // MMU Helper Functions
+ 
+
+void init_memory(void);
+int find_free_frame(void);
+
+#endif
